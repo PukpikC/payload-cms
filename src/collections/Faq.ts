2a9e5d9283de3d1a6213ddd79f137d3c51ecd825
David Gray <dacgray@gmail.com>
2023-03-06 14:25:02 +0700

    wip

--- src/collections/Faq.ts
+++ src/collections/Faq.ts
@@ -2,11 +2,13 @@ import {CollectionConfig} from 'payload/types';
 
 const Faq: CollectionConfig = {
     slug: 'faq',
-    auth: {
-        useAPIKey: true
-    },
     admin: {
         useAsTitle: 'id',
+    },
+    access: {
+        read: ({ req: { user } }) => {
+            return true;
+        },
     },
     fields: [
         {
@@ -29,14 +31,43 @@ const Faq: CollectionConfig = {
             type: 'text',
         },
         {
-            name: 'Q1',
-            localized: true,
-            type: 'text',
+            name: 'time', // required
+            type: 'date', // required
+            label: 'Event Start Time',
+            admin: {
+                date: {
+                    // All config options above should be placed here
+                    pickerAppearance: 'timeOnly',
+                }
+            }
         },
         {
-            name: 'A1',
+            name: 'something',
             localized: true,
             type: 'code',
+        },
+        {
+            name: 'questions_and_answers', // required
+            type: 'array', // required
+            label: 'Questions and Answers',
+            minRows: 2,
+            maxRows: 10,
+            labels: {
+                singular: 'Question and Answer',
+                plural: 'Questions and Answers',
+            },
+            fields: [ // required
+                {
+                    name: 'question',
+                    localized: true,
+                    type: 'text',
+                },
+                {
+                    name: 'answer',
+                    localized: true,
+                    type: 'code',
+                }
+            ],
         },
     ],
 };
--- src/collections/OldShopContent.ts
+++ src/collections/OldShopContent.ts
@@ -2,16 +2,27 @@ import {CollectionConfig} from 'payload/types';
 
 const OldShopContent: CollectionConfig = {
     slug: 'OldShopContent',
-    auth: {
-        useAPIKey: true
-    },
     admin: {
         useAsTitle: 'id',
+    },
+    access: {
+        read: ({ req: { user } }) => {
+            return true;
+        },
     },
     fields: [
         {
             name: 'id',
             type: 'text'
+        },
+        {
+            type: 'ui', // required
+            admin: {
+                components: {
+                    Field: MyCustomUIField,
+                    Cell: MyCustomUICell,
+                }
+            }
         },
         {
             name: 'title_tag',
