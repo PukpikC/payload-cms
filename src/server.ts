9ac8979964e114e23f299cd91833c588df374ef2
Pukpik <pukpic@crazy-factory.com>
2023-03-28 09:50:32 +0700

    wip: bring back db connection

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,26 @@ import payload from 'payload';
 require('dotenv').config();
 const app = express();
 
+// Test page
 app.get('/test', (_, res) => {
   res.json('hello');
 });
 
-
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
-  res.redirect('/test');
+  res.redirect('/admin');
 });
 
 const start = async () => {
   // Initialize Payload
-  // await payload.init({
-  //   secret: process.env.PAYLOAD_SECRET,
-  //   mongoURL: process.env.MONGODB_URI,
-  //   express: app,
-  //   onInit: async () => {
-  //     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-  //   },
-  // })
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    mongoURL: process.env.MONGODB_URI,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+  })
 
   // Add your own express routes here
 
